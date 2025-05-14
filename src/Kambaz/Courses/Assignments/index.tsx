ee735736fc638d074c838e90d8b3fbe9cda7fd06
Mithun <102860039+Mithun3110@users.noreply.github.com>
2025-05-14 05:54:53 +0530

    Updated assignment section

--- src/Kambaz/Courses/Assignments/index.tsx
+++ src/Kambaz/Courses/Assignments/index.tsx
@@ -8,14 +8,27 @@ export default function Assignments() {
       <h3 id="wd-assignments-title">
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
-        <li className="wd-assignment-list-item">
-          <a href="#/Kambaz/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
-            A1 - ENV + HTML
-          </a> </li>
-        <li className="wd-assignment-list-item">
-          {/* Complete On Your Own */}
-        </li>
-      </ul>
+          <li className="wd-assignment-list-item">
+            <a className="wd-assignment-link"
+              href="#/Kanbas/Courses/1234/Assignments/123">
+              A1 - ENV + HTML
+            </a>
+            <p>Multiple Modules | Not available until May 7 at 12:00am | <b>Due</b> May 13 at 11:59pm | 100pts</p>
+          </li>
+          <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link"
+            href="#/Kanbas/Courses/124/Assignments/124">
+            A2 - CSS + BOOTSTRAP
+          </a>
+          <p>Multiple Modules | Not available until May 13 at 12:00am | <b>Due</b> May 20 at 11:59pm | 100pts</p>
+          </li>
+          <li className="wd-assignment-list-item">
+          <a className="wd-assignment-link"
+            href="#/Kanbas/Courses/125/Assignments/125">
+            A3 - JAVASCRIPT + REACT
+          </a>
+          <p>Multiple Modules | Not available until May 20 at 12:00am | <b>Due</b> May 27 at 11:59pm | 100pts</p>
+          </li>
+        </ul>
     </div>
 );}
