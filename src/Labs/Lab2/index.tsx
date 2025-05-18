5870242b1ff06f5bdf71d6bd97460f6fec915550
Mithun <102860039+Mithun3110@users.noreply.github.com>
2025-05-18 16:43:01 +0530

    Lab Done

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -14,6 +14,10 @@ import ReactIconsSampler from "./ReactIcons";
 import { Container } from "react-bootstrap";
 import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
@@ -85,6 +89,10 @@ export default function Lab2() {
       </Container>
       <BootstrapGrids />
       <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
 
     </div>
   );
