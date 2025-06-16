f652aaab7b5aa7e7ae23183f0d123a0555ee5f07
Mithun <102860039+Mithun3110@users.noreply.github.com>
2025-06-16 13:27:08 +0530

    A6 first commit

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,54 +1,34 @@
-import { useParams } from "react-router-dom";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import * as db from "../../Database";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
+export default function PeopleTable({ users = [] }: { users?: any[] }) {
+ return (
+  <div id="wd-people-table">
+    <PeopleDetails />
+   <Table striped>
+    <thead>
+     <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+    </thead>
+    <tbody>
+  {users
+    .map((user: any) => (
+      <tr key={user._id}>
+        <td className="wd-full-name text-nowrap">
+          <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
+          <FaUserCircle className="me-2 fs-1 text-secondary" />
+          <span className="wd-first-name">{user.firstName}</span>
+          <span className="wd-last-name">{user.lastName}</span>
+          </Link>
+        </td>
+        <td className="wd-login-id">{user.loginId}</td>
+        <td className="wd-section">{user.section}</td>
+        <td className="wd-role">{user.role}</td>
+        <td className="wd-last-activity">{user.lastActivity}</td>
+        <td className="wd-total-activity">{user.totalActivity}</td>
+      </tr>
+    ))}
+</tbody>
 
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users, enrollments } = db;
-
-  const enrolledUsers = users.filter((usr) =>
-    enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
-  );
-
-  return (
-    <div id="wd-people-table" className="p-3">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Login ID</th>
-            <th>Section</th>
-            <th>Role</th>
-            <th>Last Activity</th>
-            <th>Total Activity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {enrolledUsers.length > 0 ? (
-            enrolledUsers.map((user) => (
-              <tr key={user._id}>
-                <td className="wd-full-name text-nowrap">
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}</span>{" "}
-                  <span className="wd-last-name">{user.lastName}</span>
-                </td>
-                <td className="wd-login-id">{user.loginId}</td>
-                <td className="wd-section">{user.section}</td>
-                <td className="wd-role">{user.role}</td>
-                <td className="wd-last-activity">{user.lastActivity}</td>
-                <td className="wd-total-activity">{user.totalActivity}</td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={6} className="text-center">
-                No users enrolled in this course.
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-    </div>
-  );
-}
+   </Table>
+  </div> );}
